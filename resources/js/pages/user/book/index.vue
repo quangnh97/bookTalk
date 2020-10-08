9332c03fb1cd43af4fb38c08310e2db36b964ffd
quangnh97 <quang10397@gmail.com>
2020-10-08 21:42:15 +0700

    book

---
+++ resources/js/pages/user/book/index.vue
@@ -0,0 +1,32 @@
+<template>
+<div class="col-md-9 col-sm-12 col-xs-12 center-con">
+
+</div>
+</template>
+
+<script>
+export default {
+    props: {
+      auth: {
+        type: Object
+      }
+    },
+
+    data() {
+        return {
+        }
+    },
+
+    ready: function() {
+      this.created();
+    },
+
+    created() {
+
+    },
+
+    methods: {
+
+    }
+}
+</script>
